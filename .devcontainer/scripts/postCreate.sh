a971f26beafbac4322a9c67fae677f69794c1d08
Charlie Bini <5003326+cbini@users.noreply.github.com>
2024-01-08 16:02:42 +0000

    chore: project fiels

--- .devcontainer/scripts/postCreate.sh
+++ .devcontainer/scripts/postCreate.sh
@@ -2,13 +2,13 @@
 
 # update/install apt packages
 sudo apt-get -y --no-install-recommends update &&
-  sudo apt-get -y --no-install-recommends upgrade &&
-  sudo apt-get -y --no-install-recommends install bash-completion &&
-  sudo apt-get -y autoremove &&
-  sudo apt-get -y clean
+	sudo apt-get -y --no-install-recommends upgrade &&
+	sudo apt-get -y --no-install-recommends install bash-completion &&
+	sudo apt-get -y autoremove &&
+	sudo apt-get -y clean
 
 # create env folder
 mkdir -p ./env
 
-# update pip
-python -m pip install --no-cache-dir --upgrade pip
+# install pdm dependencies
+pdm install --no-lock
